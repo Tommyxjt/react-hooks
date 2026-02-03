eae2d792d894006926fca505471d9efbc4d77bdc
Tommy Xu （徐锦涛） <Tommy.Xu@merkle.com>
2026-02-03 15:00:01 +0800

    feat: useBoolean hook dev

---
+++ packages/hooks/src/useBoolean/demo/basic.tsx
@@ -0,0 +1,25 @@
+/**
+ * title: 基础用法
+ * description: 用于 boolean 切换
+ */
+
+import React from 'react'; // 这边导入 React 是因为 JSX 需要 React 作用域
+import { Button } from 'antd';
+import { useBoolean } from 'txHooks';
+
+export default () => {
+  const [state, { toggle, setTrue, setFalse }] = useBoolean(false);
+
+  return (
+    <div>
+      <p>Effects：{`${state}`}</p>
+      <p>
+        <Button onClick={toggle}>Toggle</Button>
+        <Button onClick={setTrue} style={{ margin: '0 8px' }}>
+          setTrue
+        </Button>
+        <Button onClick={setFalse}>setFalse</Button>
+      </p>
+    </div>
+  );
+};
